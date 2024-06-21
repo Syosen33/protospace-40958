5d81d65aef84a890030dedaaddc04c9261a8aa4c
Syosen33 <syosen33@gmail.com>
2024-06-21 18:26:33 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
+  resources :users, only: [:edit, :update]
+  resources :phototypes, only: [:new, :create, :destroy] do
+    resources :comments, only: [:index, :create]
+  end
 end
